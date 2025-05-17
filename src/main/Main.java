1179548a553716fe6316f0317f9766e498d6a680
Wasley Carvalho <88601440+wasleyoc@users.noreply.github.com>
2025-05-16 23:35:31 -0300

    Create Project Register Pets

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import menu.Menu;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)  {
+        Menu menu = new Menu();
+        menu.Menu();
+    }
+}
